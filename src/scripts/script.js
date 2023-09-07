41c72258e4a01e0af9cb1ab9aa67a52c1ec6d433
Mochammad Kevin Ariobimo <kevinariobimo@gmail.com>
2023-09-07 22:17:22 +0700

    testing showcase pt.2

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -25,34 +25,4 @@ $(document).ready(function(){
             }
         }
     });
-
-    // Add a new class to this script
-    $(".owl-carousel").addClass("custom-owl-carousel");
-
-    // Initialize the custom settings for the new class
-    $(".custom-owl-carousel").owlCarousel({
-        loop:true,
-        margin:20, // Customize margin
-        nav:true, // Enable navigation arrows
-        dots:true, // Enable dots navigation
-        autoplay:true,
-        autoplayTimeout:5000, // Customize autoplay timeout
-        autoplayHoverPause:true,
-        navText: [
-            "<i class='fa fa-chevron-left'></i>", // Customize left arrow icon
-            "<i class='fa fa-chevron-right'></i>" // Customize right arrow icon
-        ],
-        responsive:{
-            0:{
-                items:1,
-            },
-            768:{
-                items:2
-            },
-            1024:{
-                items:4, // Customize the number of items to display
-                center: false // Disable center mode
-            }
-        }
-    });
 });
