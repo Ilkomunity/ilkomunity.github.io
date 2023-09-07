73319ba2c061688486ee7c4801b39b480352edbb
Mochammad Kevin Ariobimo <kevinariobimo@gmail.com>
2023-09-07 15:55:15 +0700

    test push semua

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-    readonly STRAPI_URL: string;
-    readonly STRAPI_TOKEN: string;
-}
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,5 @@
-// Owlcarousel
 $(document).ready(function(){
+    // Initialize the original owl-carousel
     $(".owl-carousel").owlCarousel({
         loop:true,
         margin:10,
@@ -22,6 +22,36 @@ $(document).ready(function(){
             1024:{
                 items:3,
                 center: true
+            }
+        }
+    });
+
+    // Add a new class to this script
+    $(".owl-carousel").addClass("custom-owl-carousel");
+
+    // Initialize the custom settings for the new class
+    $(".custom-owl-carousel").owlCarousel({
+        loop:true,
+        margin:20, // Customize margin
+        nav:true, // Enable navigation arrows
+        dots:true, // Enable dots navigation
+        autoplay:true,
+        autoplayTimeout:5000, // Customize autoplay timeout
+        autoplayHoverPause:true,
+        navText: [
+            "<i class='fa fa-chevron-left'></i>", // Customize left arrow icon
+            "<i class='fa fa-chevron-right'></i>" // Customize right arrow icon
+        ],
+        responsive:{
+            0:{
+                items:1,
+            },
+            768:{
+                items:2
+            },
+            1024:{
+                items:4, // Customize the number of items to display
+                center: false // Disable center mode
             }
         }
     });
