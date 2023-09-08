8aacb78710d04b8fe0fd3d1dccad18492a67cfb6
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-09-08 09:06:40 +0700

    specify owl carousel class

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Initialize the original owl-carousel
-    $(".owl-carousel").owlCarousel({
+    $(".owl-carousel-achievement").owlCarousel({
         loop:true,
         margin:10,
         nav:false,
@@ -8,6 +8,32 @@ $(document).ready(function(){
         autoplay:true,
         autoplayTimeout:3000,
         autoplayHoverPause:true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive:{
+            0:{
+                items:1,
+            },
+            768:{
+                items:2
+            },
+            1024:{
+                items:3,
+                center: true
+            }
+        }
+    });
+    $(".owl-carousel-showcase").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false, 
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        slideBy: 3,
         navText: [
             "<i class='fa fa-angle-left'></i>",
             "<i class='fa fa-angle-right'></i>"
