268c6942bc0f5ec338bdd6b1d504b36316b1c505
Mochammad Kevin Ariobimo <kevinariobimo@gmail.com>
2023-09-19 13:34:46 +0700

    update user review nav dots

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,84 +1,81 @@
-$(document).ready(function(){
-    // Initialize the original owl-carousel
-    $(".owl-carousel-achievement").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        dots:false, 
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-        navText: [
-            "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
-        ],
-        responsive:{
-            0:{
-                items:1,
-            },
-            768:{
-                items:2
-            },
-            1024:{
-                items:3,
-                center: true
-            }
-        }
-    });
-    $(".owl-carousel-showcase").owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        dots:false, 
-        autoplay:false,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-        slideBy: 3,
-        mouseDrag: false,
-        navText: [
-            "<div class='showcase-arrow'> <img src='showcase-products-arrow-left.svg'>",
-            "<div class='showcase-arrow'> <img src='showcase-products-arrow-right.svg'>"
-        ],
-        
-        responsive:{
-            0:{
-                items:1,
-            },
-            768:{
-                items:2,
-                margin: 30
-            },
-            1024:{
-                items:3,
-                center: true
-            }
-        }
-    });
-        $(".owl-carousel-review").owlCarousel({
-            loop:true,
-            margin:0,
-            nav:true,
-            dots:false, 
-            autoplay:false,
-            autoplayTimeout:3000,
-            autoplayHoverPause:true,
-            slideBy: 5,
-            mouseDrag: false,
-            navText: [
-                "<img src='arrow-left.svg'>",
-                "<img src='arrow-right.svg'>"
-            ],
-            responsive:{
-                0:{
-                    items:2,
-                },
-                768:{
-                    items:3,
-                    margin: 0
-                },
-                1024:{
-                    items:5
-                }
-            }
-        });
+$(document).ready(function () {
+  // Initialize the original owl-carousel
+  $(".owl-carousel-achievement").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 3,
+        center: true,
+      },
+    },
+  });
+  $(".owl-carousel-showcase").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    slideBy: 3,
+    mouseDrag: false,
+    navText: [
+      "<div class='showcase-arrow'> <img src='showcase-products-arrow-left.svg'>",
+      "<div class='showcase-arrow'> <img src='showcase-products-arrow-right.svg'>",
+    ],
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+        margin: 30,
+      },
+      1024: {
+        items: 3,
+        center: true,
+      },
+    },
+  });
+  $(".owl-carousel-review").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    slideBy: 5,
+    mouseDrag: false,
+    navText: ["<img src='arrow-left.svg'>", "<img src='arrow-right.svg'>"],
+    responsive: {
+      0: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+        margin: 0,
+      },
+      1024: {
+        items: 5,
+      },
+    },
+  });
 });
